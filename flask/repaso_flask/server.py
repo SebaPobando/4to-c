937c09fbcc5f1c084104edfe068728eefe3ec53f
Sr Jengibre <spobleteobando@gmail.com>
2025-09-11 14:49:10 -0300

    Fin repaso flask

--- flask/repaso_flask/server.py
+++ flask/repaso_flask/server.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html', nombre = 'Mundo', cantidad = 5)
+
+@app.route('/saludo/<nombre>')
+def saludo(nombre):
+    return f'Hola, {nombre}'
+
+@app.route('/saludo/<nombre>/<int:edad>')
+def saludo_con_edad(nombre, edad):
+    return f'Hola, {nombre}. Tienes {edad} años.'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
